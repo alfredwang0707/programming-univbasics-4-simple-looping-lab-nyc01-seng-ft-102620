2aae80dad347508b1af173ddcc2984d4b771329c
Alfred Wang <alfredwang0707@gmail.com>
2020-09-30 20:33:39 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,9 +25,9 @@ end
 
 def return_string_array(array)
   count = 0 
-  new array =[]
+  new-array =[]
   while count < array.length do 
-    new array.push(array[count].to_s)
+    new_array.push(array[count].to_s)
     count += 1 
   end 
 end
