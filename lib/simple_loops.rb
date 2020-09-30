f0d8eef2a69e9230376e95553480d947d7d7c755
Alfred Wang <alfredwang0707@gmail.com>
2020-09-30 20:13:58 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,7 +4,7 @@ def loop_message_five_times(message)
 puts message
 count += 1 
  end 
-end 
+
 
 loop_message_n_times(message, n)
 count = 0 
@@ -12,4 +12,4 @@ while count < n do
   puts message
 count += 1 
  end
-end
+
